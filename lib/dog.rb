be2dbef4208ceb26458641216720d5c1be8b18a0
Abel Kalum <chumbabel@gmail.com>
2020-01-29 18:42:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,24 @@
-class Dog
-  def initialize(name, breed)
-    @name = "Fido"
-    @breed = "Pug"
+class Dog 
+
+  def initialize(name, breed="Mut")
+    @name = name
+    @breed = breed
   end
- else @breed = "Mutt"
-  puts 
+
+  def name=(name)
+    @name = name
+  end
+
+  def name 
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
 end
